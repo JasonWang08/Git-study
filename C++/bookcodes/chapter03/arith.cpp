5cc4d36fb4037c43408ffd41dae385582bcf6e6c
Heng <3086595625wang@gmail.com>
2024-07-28 11:16:07 +0800

    nothing

---
+++ C++/bookcodes/chapter03/arith.cpp
@@ -0,0 +1,24 @@
+// arith.cpp -- some C++ arithmetic
+#include <iostream>
+#include <iomanip>
+int main()
+{
+    using namespace std;
+    float hats, heads;
+    //double hats, heads;
+    
+    cout.setf(ios_base::fixed, ios_base::floatfield); // fixed-point
+    cout << "Enter a number: ";
+    cin >> hats;
+    cout << "Enter another number: ";
+    cin >> heads;
+    cout<<setprecision(2);
+    cout << "hats = " << hats << "; heads = " << heads << endl;
+    cout << "hats + heads = " << hats + heads << endl;
+    cout << "hats - heads = " << hats - heads << endl;
+    cout << "hats * heads = " << hats * heads << endl;
+    cout << "hats / heads = " << hats / heads << endl;
+    // cin.get();
+    // cin.get();
+    return 0;
+}
